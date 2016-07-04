cb62064307a8e5fc1447a8ed4b5d89a876bb4319
Georgy Buranov <gburanov@gmail.com>
2016-07-04 13:28:21 +0200

    install atom packages

--- install.sh
+++ install.sh
@@ -8,3 +8,7 @@ cp -R $DIR/.oh-my-zsh/* $HOME/.oh-my-zsh/
 cp -R $DIR/.vim/* $HOME/.vim/
 
 cp -R $DIR/fonts/* $HOME/Library/Fonts/
+
+
+# Install atom packages
+apm install --packages-file atom-packages.txt
