126d0ef4d2152ce4da3b4a3f3172c08f35186d58
Georgy Buranov <gburanov@gmail.com>
2025-10-26 10:42:11 +0100

    cleanup

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -x #echo on
 
-#Install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 # Install apps I need
 brew install zsh
 brew install --cask coconutbattery
@@ -13,13 +10,6 @@ brew install --cask spotify
 brew install --cask telegram
 brew install --cask textmate
 brew install --cask vlc
-
-shopt -s dotglob;
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cp -R $DIR/.oh-my-zsh/* $HOME/.oh-my-zsh/
-cp -R $DIR/.vim/* $HOME/.vim/
-cp  $DIR/.gitconfig $HOME
 
 #Fonts
 brew install --cask font-jetbrains-mono
