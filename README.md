5ee6f9ea12a3dbad7257dfd0507a5a8d0ec817e1
Georgy Buranov <gburanov@gmail.com>
2025-10-26 10:11:14 +0100

    Update dotfiles

--- README.md
+++ README.md
@@ -1,17 +1,5 @@
-# Update validity from time to time
-```
-ls ~/.atom/packages > ./atom-packages.txt
-```
-
 # Do the install
 
-./basics.sh
+```
 ./install.sh
-
-* Set the ./zshrc ZSH_THEME="gburanov"
-
-## List of additional software
-* Textmate
-* VLC
-* Telegram
-* Spofity
+```
--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -x #echo on
 
+#Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install apps I need
+brew install zsh
+brew install --cask coconutbattery
+brew install --cask ghostty
+brew install --cask fbreader
+brew install --cask spotify
+brew install --cask telegram
+brew install --cask textmate
+brew install --cask vlc
+
 shopt -s dotglob;
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -8,7 +21,6 @@ cp -R $DIR/.oh-my-zsh/* $HOME/.oh-my-zsh/
 cp -R $DIR/.vim/* $HOME/.vim/
 cp  $DIR/.gitconfig $HOME
 
-cp -R $DIR/fonts/* $HOME/Library/Fonts/
+#Fonts
+brew install --cask font-jetbrains-mono
 
-# Install atom packages
-apm install --packages-file atom-packages.txt
